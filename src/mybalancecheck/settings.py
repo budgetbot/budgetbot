ffdc4c9bbe3ffa81fbe484778fda3f87b1147057
Steve Winton <stevewinton@gmail.com>
2015-05-11 11:30:23 -0500

    Heroku-friendly settings, I think.

--- src/mybalancecheck/settings.py
+++ src/mybalancecheck/settings.py
@@ -1,7 +1,9 @@
+import os
+
 # Default timezone
-TZ = "UTC"
+TZ = os.environ.get("TZ", "CST")
 
 # Used by mybalancecheck.backends.gspreadsheet.GSpreadsheetBackend to authenticate with Google Spreadsheets
 # For steps to generate, see: https://gspread.readthedocs.org/en/latest/oauth2.html
-GSPREAD_AUTH_EMAIL = ""
-GSPREAD_AUTH_KEY = ""
+GSPREAD_AUTH_EMAIL = os.environ.get("GSPREAD_AUTH_EMAIL")
+GSPREAD_AUTH_KEY = os.environ.get("GSPREAD_AUTH_KEY")
