524786a0b5184473612dc23dd79fc2971b046ab0
Steve Winton <steve.winton@gmail.com>
2015-05-10 20:01:36 -0500

    First commit

---
+++ scratch.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+From: https://gspread.readthedocs.org/en/latest/oauth2.html
+"""
+
+import json
+import gspread
+from oauth2client.client import SignedJwtAssertionCredentials
+
+json_key = json.load(open('My Balance Check-74d33539077f.json'))
+scope = ['https://spreadsheets.google.com/feeds']
+
+credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
+gc = gspread.authorize(credentials)
+
+wks = gc.open("Groceries").sheet1
+
+# print wks
