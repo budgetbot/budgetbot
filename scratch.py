3bc02b8583f8dbe5926926cd2c023e514a83007e
Steve Winton <steve.winton@gmail.com>
2015-05-10 21:40:33 -0500

    Adding transactions, getting balance.

--- scratch.py
+++ scratch.py
@@ -5,7 +5,10 @@ From: https://gspread.readthedocs.org/en/latest/oauth2.html
 """
 
 import json
+import re
 import gspread
+import arrow
+
 from oauth2client.client import SignedJwtAssertionCredentials
 
 json_key = json.load(open('My Balance Check-74d33539077f.json'))
@@ -16,4 +19,11 @@ gc = gspread.authorize(credentials)
 
 wks = gc.open("Groceries").sheet1
 
-# print wks
+# Current balance
+data = wks.acell("A2").value
+regex = re.compile(r'\$(\d+\.\d+)')
+print float(regex.search(data).groups()[0])
+
+# Add tx
+now = arrow.now("CST").datetime
+wks.append_row((15.0, "Publix", "/".join(map(str, (now.month, now.day, now.year)))))
